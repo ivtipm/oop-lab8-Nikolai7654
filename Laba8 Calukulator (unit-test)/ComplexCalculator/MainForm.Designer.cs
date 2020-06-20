79791445b75473e2b3b26582e250702174775862
Николай Меркульев <55264259+Nikolai7654@users.noreply.github.com>
2020-06-20 08:13:41 +0300

    Мекульев николай

--- Laba8 Calukulator (unit-test)/ComplexCalculator/MainForm.Designer.cs
+++ Laba8 Calukulator (unit-test)/ComplexCalculator/MainForm.Designer.cs
@@ -43,26 +43,28 @@
             this.ArgumentLabel = new System.Windows.Forms.Label();
             this.LeftNumModuleText = new System.Windows.Forms.Label();
             this.RightNumModuleText = new System.Windows.Forms.Label();
-            this.LeftNumArgText = new System.Windows.Forms.Label();
             this.RightNumArgText = new System.Windows.Forms.Label();
             this.LeftNumModuleLabel = new System.Windows.Forms.Label();
             this.LeftNumArgLabel = new System.Windows.Forms.Label();
             this.RightNumModuleLabel = new System.Windows.Forms.Label();
             this.RightNumArgLabel = new System.Windows.Forms.Label();
+            this.LeftNumArgText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LeftNumber
             // 
-            this.LeftNumber.Location = new System.Drawing.Point(64, 50);
+            this.LeftNumber.Location = new System.Drawing.Point(85, 62);
+            this.LeftNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LeftNumber.Name = "LeftNumber";
-            this.LeftNumber.Size = new System.Drawing.Size(117, 20);
+            this.LeftNumber.Size = new System.Drawing.Size(155, 22);
             this.LeftNumber.TabIndex = 0;
             // 
             // RightNumber
             // 
-            this.RightNumber.Location = new System.Drawing.Point(430, 50);
+            this.RightNumber.Location = new System.Drawing.Point(337, 62);
+            this.RightNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RightNumber.Name = "RightNumber";
-            this.RightNumber.Size = new System.Drawing.Size(117, 20);
+            this.RightNumber.Size = new System.Drawing.Size(155, 22);
             this.RightNumber.TabIndex = 1;
             // 
             // ActionList
@@ -73,18 +75,20 @@
             "Вычесть",
             "Умножить",
             "Поделить"});
-            this.ActionList.Location = new System.Drawing.Point(248, 49);
+            this.ActionList.Location = new System.Drawing.Point(248, 60);
+            this.ActionList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ActionList.Name = "ActionList";
-            this.ActionList.Size = new System.Drawing.Size(121, 21);
+            this.ActionList.Size = new System.Drawing.Size(80, 24);
             this.ActionList.TabIndex = 2;
             this.ActionList.Text = "Сложить";
             // 
             // CalculateButton
             // 
             this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CalculateButton.Location = new System.Drawing.Point(248, 105);
+            this.CalculateButton.Location = new System.Drawing.Point(79, 127);
+            this.CalculateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(121, 23);
+            this.CalculateButton.Size = new System.Drawing.Size(161, 28);
             this.CalculateButton.TabIndex = 3;
             this.CalculateButton.Text = "РАССЧИТАТЬ";
             this.CalculateButton.UseVisualStyleBackColor = true;
@@ -94,45 +98,50 @@
             // 
             this.ResultText.AutoSize = true;
             this.ResultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ResultText.Location = new System.Drawing.Point(261, 144);
+            this.ResultText.Location = new System.Drawing.Point(83, 174);
+            this.ResultText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ResultText.Name = "ResultText";
-            this.ResultText.Size = new System.Drawing.Size(93, 20);
+            this.ResultText.Size = new System.Drawing.Size(117, 25);
             this.ResultText.TabIndex = 4;
             this.ResultText.Text = "Результат:";
             // 
             // LeftNumberText
             // 
             this.LeftNumberText.AutoSize = true;
-            this.LeftNumberText.Location = new System.Drawing.Point(64, 31);
+            this.LeftNumberText.Location = new System.Drawing.Point(85, 38);
+            this.LeftNumberText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LeftNumberText.Name = "LeftNumberText";
-            this.LeftNumberText.Size = new System.Drawing.Size(48, 13);
+            this.LeftNumberText.Size = new System.Drawing.Size(61, 17);
             this.LeftNumberText.TabIndex = 5;
             this.LeftNumberText.Text = "Число 1";
             // 
             // RightNumberText
             // 
             this.RightNumberText.AutoSize = true;
-            this.RightNumberText.Location = new System.Drawing.Point(430, 31);
+            this.RightNumberText.Location = new System.Drawing.Point(350, 41);
+            this.RightNumberText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RightNumberText.Name = "RightNumberText";
-            this.RightNumberText.Size = new System.Drawing.Size(48, 13);
+            this.RightNumberText.Size = new System.Drawing.Size(61, 17);
             this.RightNumberText.TabIndex = 6;
             this.RightNumberText.Text = "Число 2";
             // 
             // ActionText
             // 
             this.ActionText.AutoSize = true;
-            this.ActionText.Location = new System.Drawing.Point(248, 31);
+            this.ActionText.Location = new System.Drawing.Point(245, 39);
+            this.ActionText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ActionText.Name = "ActionText";
-            this.ActionText.Size = new System.Drawing.Size(57, 13);
+            this.ActionText.Size = new System.Drawing.Size(75, 17);
             this.ActionText.TabIndex = 7;
             this.ActionText.Text = "Операция";
             // 
             // ResultLabel
             // 
             this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ResultLabel.Location = new System.Drawing.Point(67, 186);
+            this.ResultLabel.Location = new System.Drawing.Point(75, 212);
+            this.ResultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ResultLabel.Name = "ResultLabel";
-            this.ResultLabel.Size = new System.Drawing.Size(480, 23);
+            this.ResultLabel.Size = new System.Drawing.Size(202, 28);
             this.ResultLabel.TabIndex = 8;
             this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -140,9 +149,10 @@
             // 
             this.ModuleText.AutoSize = true;
             this.ModuleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ModuleText.Location = new System.Drawing.Point(12, 223);
+            this.ModuleText.Location = new System.Drawing.Point(16, 274);
+            this.ModuleText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ModuleText.Name = "ModuleText";
-            this.ModuleText.Size = new System.Drawing.Size(72, 20);
+            this.ModuleText.Size = new System.Drawing.Size(88, 25);
             this.ModuleText.TabIndex = 9;
             this.ModuleText.Text = "Модуль:";
             // 
@@ -150,9 +160,10 @@
             // 
             this.ArgumentText.AutoSize = true;
             this.ArgumentText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ArgumentText.Location = new System.Drawing.Point(303, 223);
+            this.ArgumentText.Location = new System.Drawing.Point(243, 272);
+            this.ArgumentText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ArgumentText.Name = "ArgumentText";
-            this.ArgumentText.Size = new System.Drawing.Size(85, 20);
+            this.ArgumentText.Size = new System.Drawing.Size(110, 25);
             this.ArgumentText.TabIndex = 10;
             this.ArgumentText.Text = "Аргумент:";
             // 
@@ -160,9 +171,10 @@
             // 
             this.ModuleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ModuleLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ModuleLabel.Location = new System.Drawing.Point(90, 223);
+            this.ModuleLabel.Location = new System.Drawing.Point(120, 274);
+            this.ModuleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ModuleLabel.Name = "ModuleLabel";
-            this.ModuleLabel.Size = new System.Drawing.Size(207, 23);
+            this.ModuleLabel.Size = new System.Drawing.Size(120, 28);
             this.ModuleLabel.TabIndex = 11;
             this.ModuleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -170,81 +182,91 @@
             // 
             this.ArgumentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ArgumentLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ArgumentLabel.Location = new System.Drawing.Point(397, 220);
+            this.ArgumentLabel.Location = new System.Drawing.Point(363, 274);
+            this.ArgumentLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ArgumentLabel.Name = "ArgumentLabel";
-            this.ArgumentLabel.Size = new System.Drawing.Size(182, 23);
+            this.ArgumentLabel.Size = new System.Drawing.Size(78, 28);
             this.ArgumentLabel.TabIndex = 12;
             this.ArgumentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // LeftNumModuleText
             // 
             this.LeftNumModuleText.AutoSize = true;
-            this.LeftNumModuleText.Location = new System.Drawing.Point(64, 73);
+            this.LeftNumModuleText.Location = new System.Drawing.Point(85, 90);
+            this.LeftNumModuleText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LeftNumModuleText.Name = "LeftNumModuleText";
-            this.LeftNumModuleText.Size = new System.Drawing.Size(48, 13);
+            this.LeftNumModuleText.Size = new System.Drawing.Size(61, 17);
             this.LeftNumModuleText.TabIndex = 13;
             this.LeftNumModuleText.Text = "Модуль:";
             // 
             // RightNumModuleText
             // 
             this.RightNumModuleText.AutoSize = true;
-            this.RightNumModuleText.Location = new System.Drawing.Point(430, 73);
+            this.RightNumModuleText.Location = new System.Drawing.Point(364, 90);
+            this.RightNumModuleText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RightNumModuleText.Name = "RightNumModuleText";
-            this.RightNumModuleText.Size = new System.Drawing.Size(48, 13);
+            this.RightNumModuleText.Size = new System.Drawing.Size(61, 17);
             this.RightNumModuleText.TabIndex = 14;
             this.RightNumModuleText.Text = "Модуль:";
-            // 
-            // LeftNumArgText
-            // 
-            this.LeftNumArgText.AutoSize = true;
-            this.LeftNumArgText.Location = new System.Drawing.Point(64, 86);
-            this.LeftNumArgText.Name = "LeftNumArgText";
-            this.LeftNumArgText.Size = new System.Drawing.Size(58, 13);
-            this.LeftNumArgText.TabIndex = 15;
-            this.LeftNumArgText.Text = "Аргумент:";
             // 
             // RightNumArgText
             // 
             this.RightNumArgText.AutoSize = true;
-            this.RightNumArgText.Location = new System.Drawing.Point(430, 86);
+            this.RightNumArgText.Location = new System.Drawing.Point(364, 107);
+            this.RightNumArgText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RightNumArgText.Name = "RightNumArgText";
-            this.RightNumArgText.Size = new System.Drawing.Size(58, 13);
+            this.RightNumArgText.Size = new System.Drawing.Size(73, 17);
             this.RightNumArgText.TabIndex = 16;
             this.RightNumArgText.Text = "Аргумент:";
             // 
             // LeftNumModuleLabel
             // 
-            this.LeftNumModuleLabel.Location = new System.Drawing.Point(118, 73);
+            this.LeftNumModuleLabel.Location = new System.Drawing.Point(157, 90);
+            this.LeftNumModuleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LeftNumModuleLabel.Name = "LeftNumModuleLabel";
-            this.LeftNumModuleLabel.Size = new System.Drawing.Size(73, 13);
+            this.LeftNumModuleLabel.Size = new System.Drawing.Size(97, 16);
             this.LeftNumModuleLabel.TabIndex = 17;
             // 
             // LeftNumArgLabel
             // 
-            this.LeftNumArgLabel.Location = new System.Drawing.Point(128, 86);
+            this.LeftNumArgLabel.Location = new System.Drawing.Point(171, 106);
+            this.LeftNumArgLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LeftNumArgLabel.Name = "LeftNumArgLabel";
-            this.LeftNumArgLabel.Size = new System.Drawing.Size(63, 13);
+            this.LeftNumArgLabel.Size = new System.Drawing.Size(84, 16);
             this.LeftNumArgLabel.TabIndex = 18;
             // 
             // RightNumModuleLabel
             // 
-            this.RightNumModuleLabel.Location = new System.Drawing.Point(484, 73);
+            this.RightNumModuleLabel.Location = new System.Drawing.Point(445, 91);
+            this.RightNumModuleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RightNumModuleLabel.Name = "RightNumModuleLabel";
-            this.RightNumModuleLabel.Size = new System.Drawing.Size(63, 13);
+            this.RightNumModuleLabel.Size = new System.Drawing.Size(84, 16);
             this.RightNumModuleLabel.TabIndex = 19;
             // 
             // RightNumArgLabel
             // 
-            this.RightNumArgLabel.Location = new System.Drawing.Point(494, 86);
+            this.RightNumArgLabel.Location = new System.Drawing.Point(445, 108);
+            this.RightNumArgLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RightNumArgLabel.Name = "RightNumArgLabel";
-            this.RightNumArgLabel.Size = new System.Drawing.Size(63, 13);
+            this.RightNumArgLabel.Size = new System.Drawing.Size(84, 16);
             this.RightNumArgLabel.TabIndex = 20;
+            // 
+            // LeftNumArgText
+            // 
+            this.LeftNumArgText.AutoSize = true;
+            this.LeftNumArgText.Location = new System.Drawing.Point(85, 106);
+            this.LeftNumArgText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LeftNumArgText.Name = "LeftNumArgText";
+            this.LeftNumArgText.Size = new System.Drawing.Size(73, 17);
+            this.LeftNumArgText.TabIndex = 15;
+            this.LeftNumArgText.Text = "Аргумент:";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 270);
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(535, 332);
             this.Controls.Add(this.RightNumArgLabel);
             this.Controls.Add(this.RightNumModuleLabel);
             this.Controls.Add(this.LeftNumArgLabel);
@@ -266,6 +288,7 @@
             this.Controls.Add(this.ActionList);
             this.Controls.Add(this.RightNumber);
             this.Controls.Add(this.LeftNumber);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.ShowIcon = false;
@@ -292,12 +315,12 @@
         private System.Windows.Forms.Label ArgumentLabel;
         private System.Windows.Forms.Label LeftNumModuleText;
         private System.Windows.Forms.Label RightNumModuleText;
-        private System.Windows.Forms.Label LeftNumArgText;
         private System.Windows.Forms.Label RightNumArgText;
         private System.Windows.Forms.Label LeftNumModuleLabel;
         private System.Windows.Forms.Label LeftNumArgLabel;
         private System.Windows.Forms.Label RightNumModuleLabel;
         private System.Windows.Forms.Label RightNumArgLabel;
+        private System.Windows.Forms.Label LeftNumArgText;
     }
 }
 
